42b139393edb28f6f43948acb91782f7282e2214
FoggDev <foggdev@gmail.com>
2023-10-03 14:52:44 -0600

    feat: Fixing unit tests

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -4,15 +4,15 @@ import cx from '../index'
 
 describe('cx', () => {
   it('should concatenate string arguments', () => {
-    expect(cx('class1', 'class2')).toBe('class1 class2')
+    expect(cx.join('class1', 'class2')).toBe('class1 class2')
   })
 
   it('should handle numbers as arguments', () => {
-    expect(cx('class1', 123)).toBe('class1 123')
+    expect(cx.join('class1', 123)).toBe('class1 123')
   })
 
   it('should flatten nested arrays', () => {
-    expect(cx('class1', ['nested1', 'nested2'])).toBe('class1 nested1 nested2')
+    expect(cx.join('class1', ['nested1', 'nested2'])).toBe('class1 nested1 nested2')
   })
 
   it('should include object keys with truthy values as class names', () => {
@@ -21,11 +21,11 @@ describe('cx', () => {
       'btn-primary': true,
       'btn-disabled': false
     }
-    expect(cx(buttonStyles)).toBe('btn btn-primary')
+    expect(cx.join(buttonStyles)).toBe('btn btn-primary')
   })
 
   it('should handle falsy values', () => {
-    expect(cx('class1', null, undefined, false, 'class2')).toBe('class1 class2')
+    expect(cx.join('class1', null, undefined, false, 'class2')).toBe('class1 class2')
   })
 
   it('should handle mixed types', () => {
@@ -33,10 +33,10 @@ describe('cx', () => {
       active: true,
       disabled: false
     }
-    expect(cx('class1', 123, ['nested'], styles)).toBe('class1 123 nested active')
+    expect(cx.join('class1', 123, ['nested'], styles)).toBe('class1 123 nested active')
   })
 
   it('should handle no arguments', () => {
-    expect(cx()).toBe('')
+    expect(cx.join()).toBe('')
   })
 })
