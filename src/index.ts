1c53fbee5e225c3d9c514c0ca8e6e25084ce99dd
FoggDev <foggdev@gmail.com>
2023-10-03 14:50:22 -0600

    feat: Adding extract function to cx

--- src/index.ts
+++ src/index.ts
@@ -1,42 +1,57 @@
-function cx(...classes: any): string {
-  const classArr = []
+const cx = {
+  join(...classes: any): string {
+    const classArr = []
 
-  for (let i = 0; i < classes.length; i += 1) {
-    const arg: any = classes[i]
+    for (let i = 0; i < classes.length; i += 1) {
+      const arg: any = classes[i]
 
-    if (!arg) {
-      continue
-    }
-
-    const type = typeof arg
-
-    if (type === 'string' || type === 'number') {
-      classArr.push(arg)
-    }
-
-    if (Array.isArray(arg)) {
-      classArr.push(cx(...arg))
-      continue
-    }
-
-    if (type === 'object') {
-      if (
-        arg.toString !== Object.prototype.toString &&
-        !arg.toString.toString().includes('[native code]')
-      ) {
-        classArr.push(arg.toString())
+      if (!arg) {
         continue
       }
 
-      for (const key in arg) {
-        if (arg[key]) {
-          classArr.push(key)
+      const type = typeof arg
+
+      if (type === 'string' || type === 'number') {
+        classArr.push(arg)
+      }
+
+      if (Array.isArray(arg)) {
+        classArr.push(cx.join(...arg))
+        continue
+      }
+
+      if (type === 'object') {
+        if (
+          arg.toString !== Object.prototype.toString &&
+          !arg.toString.toString().includes('[native code]')
+        ) {
+          classArr.push(arg.toString())
+          continue
+        }
+
+        for (const key in arg) {
+          if (arg[key]) {
+            classArr.push(key)
+          }
         }
       }
     }
-  }
 
-  return classArr.join(' ')
+    return classArr.join(' ')
+  },
+  extract(obj: any): string[] {
+    let classes: string[] = []
+
+    for (const key in obj) {
+      if (typeof obj[key] === 'string') {
+        classes = [...classes, ...obj[key].split(' ')]
+      } else {
+        classes = [...classes, ...cx.extract(obj[key])]
+      }
+    }
+
+    return classes
+  }
 }
 
 export default cx
